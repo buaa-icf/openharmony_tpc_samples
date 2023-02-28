f9908f9ab72c03b804dce098529532e9e7c08e38
zhaodongyang <zhaodongyang1@huawei.com>
2023-02-28 14:40:10 +0800

    add cpyright and lisence header
    Signed-off-by: zhaodongyang <zhaodongyang1@huawei.com>

--- PathToRegexpDemo/entry/src/main/ets/JumpPathConfig.ts
+++ PathToRegexpDemo/entry/src/main/ets/JumpPathConfig.ts
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 class JumpBean {
     showText: string = '';
     jumpPath: string = '';
@@ -86,9 +101,6 @@ export default class JumpPathConfig {
 
             let bean12 = new JumpBean('跳转CompilePage', 'pages/CompilePage');
             this.dataList.push(bean12)
-
-            let bean13 = new JumpBean('跳转WorkingTokensPage', 'pages/WorkingTokensPage');
-            this.dataList.push(bean13)
         }
     }
 }
