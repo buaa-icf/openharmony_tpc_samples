62e0d225cff3586ba778f7667e58fd2d98fca454
cuijiaojiao1 <cuijiaojiao1@huawei.com>
2023-08-20 19:37:13 +0800

    添加xml-js module依赖
    Signed-off-by: cuijiaojiao1 <cuijiaojiao1@huawei.com>

---
+++ protobuf_format/xml-js/src/main/ets/json2xml.js
@@ -0,0 +1,17 @@
+import js2xml from './js2xml';
+
+var json2xml = function (json, options) {
+  var js = null;
+  if (typeof (json) === 'string') {
+    try {
+      js = JSON.parse(json);
+    } catch (e) {
+      throw new Error('The JSON structure is invalid');
+    }
+  } else {
+    js = json;
+  }
+  return js2xml(js, options);
+};
+
+export default json2xml;
