3c15d5968c664fbbeb11dce342dc4c41f565cdee
houyoujin <houyoujin@h-partners.com>
2023-12-22 14:46:22 +0800

    修改OAT.xml、补充License、修改modul名
    Signed-off-by: houyoujin <houyoujin@h-partners.com>

--- sanitize-html/CHANGELOG.md
+++ sanitize-html/CHANGELOG.md
@@ -1,5 +1,5 @@
 ## Version 2.0.0
-1.DevEco Studio 版本： 4.1 Canary(4.1.3.317),OpenHarmony SDK:API11 (4.1.0.36)。
+1.适配DevEco Studio 版本： 4.1 Canary(4.1.3.317),OpenHarmony SDK:API11 (4.1.0.36)。
 
 ## Version 1.0.1
 1.适配DevEco Studio 3.1 Beta1版本
--- sanitize-html/library/src/main/js/index.js
+++ sanitize-html/library/src/main/js/index.js
@@ -1,32 +1,9 @@
-/**
- * MIT License
- *
- * Copyright (C) 2022 Huawei Device Co., Ltd.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-import * as htmlparser from 'htmlparser2'
+import * as htmlparser from '@ohos/htmlparser2'
 import * as escapeStringRegexp from 'escape-string-regexp'
 import {isPlainObject} from 'is-plain-object'
 import * as deepmerge from 'deepmerge'
 import * as parseSrcset from 'parse-srcset'
-import postcss from 'postcss/src/main/ets/components/lib/postcss'
+import postcss from '@ohos/postcss/src/main/ets/components/lib/postcss'
 // Tags that can conceivably represent stand-alone media.
 const mediaTags = [
   'img', 'audio', 'video', 'picture', 'svg',
