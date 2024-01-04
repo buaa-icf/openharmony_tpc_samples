9a503a8bfcccc141ab2599734f6a0e226089fa21
sunao <sunao19@h-partners.com>
2024-01-04 15:01:34 +0800

    添加适配ArkTS新语法且已发布OHPM的Rxjs Demo工程文件
    Signed-off-by: sunao <sunao19@h-partners.com>

--- RxJS/CHANGELOG.md
+++ RxJS/CHANGELOG.md
@@ -1,5 +1,4 @@
-## 1.0.1
-适配DevEco Studio 3.1 Beta1版本
 
-## 1.0.0
-RxJS Demo项目
+## 7.8.1
+添加rxjsDemo项目
+rxjs是一个通过使用可观察序列来合成异步和基于事件的程序的JavaScript库.
--- RxJS/entry/src/main/ets/log.js
+++ RxJS/entry/src/main/ets/log.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Copyright (C) 2024 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -19,40 +19,39 @@ const TAG = 'RxJs---';
  * Basic log class
  */
 export default class Log {
+    /**
+     * print info level log
+     *
+     * @param {string} log - Log needs to be printed
+     */
+    static showInfo(log, ...ms) {
+        console.info(log, ms);
+    }
 
-  /**
-   * print info level log
-   *
-   * @param {string} log - Log needs to be printed
-   */
-  static showInfo(log, ...ms) {
-    console.info(log, ms);
-  }
+    /**
+     * print debug level log
+     *
+     * @param {string} log - Log needs to be printed
+     */
+    static showDebug(log, ...ms) {
+        console.debug(log, ms);
+    }
 
-  /**
-   * print debug level log
-   *
-   * @param {string} log - Log needs to be printed
-   */
-  static showDebug(log, ...ms) {
-    console.debug(log, ms);
-  }
+    /**
+     * print error level log
+     *
+     * @param {string} log - Log needs to be printed
+     */
+    static showError(log, ...ms) {
+        console.error(log, ms);
+    }
 
-  /**
-   * print error level log
-   *
-   * @param {string} log - Log needs to be printed
-   */
-  static showError(log, ...ms) {
-    console.error(log, ms);
-  }
-
-  /**
-   * print log level log
-   *
-   * @param {string} log - Log needs to be printed
-   */
-  static showLog(log, ...ms) {
-    console.log(log, ms);
-  }
+    /**
+     * print log level log
+     *
+     * @param {string} log - Log needs to be printed
+     */
+    static showLog(log, ...ms) {
+        console.log(log, ms);
+    }
 }
