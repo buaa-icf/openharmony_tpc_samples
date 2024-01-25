fc1c382920f4d02621a347310ad151f6e7579c02
liqi <liqi219@h-partners.com>
2024-01-25 11:19:31 +0800

    feature:修改license协议
    Signed-off-by: liqi <liqi219@h-partners.com>

--- regeneratorruntime/entry/src/ohosTest/ets/testrunner/OpenHarmonyTestRunner.ts
+++ regeneratorruntime/entry/src/ohosTest/ets/testrunner/OpenHarmonyTestRunner.ts
@@ -1,7 +1,7 @@
 /**
  * MIT License
  *
- * Copyright (c) 2023 Huawei Device Co., Ltd. All rights reserved.
+ * Copyright (c) 2024 Huawei Device Co., Ltd. All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
