2bed8a97f44c2bc7ce8dbdb2b6d527b390f101aa
wupingyuan <wupingyuan2@h-partners.com>
2025-05-13 18:11:04 +0800

    Add ohos_adaptivecards component
    Signed-off-by: wupingyuan <wupingyuan2@h-partners.com>

---
+++ ohos_adaptivecards/library/src/main/ets/registry.ts
@@ -0,0 +1,75 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import { SerializableObject, Version, Versions } from "./serialization";
+
+export interface ITypeRegistration<T extends SerializableObject> {
+    typeName: string;
+    objectType: { new (): T };
+    schemaVersion: Version;
+}
+
+export class CardObjectRegistry<T extends SerializableObject> {
+    private _items: { [typeName: string]: ITypeRegistration<T> } = {};
+
+    findByName(typeName: string): ITypeRegistration<T> | undefined {
+        return this._items.hasOwnProperty(typeName) ? this._items[typeName] : undefined;
+    }
+
+    clear() {
+        this._items = {};
+    }
+
+    copyTo(target: CardObjectRegistry<T>) {
+        const keys = Object.keys(this._items);
+
+        for (const key of keys) {
+            const typeRegistration = this._items[key];
+
+            target.register(
+                typeRegistration.typeName,
+                typeRegistration.objectType,
+                typeRegistration.schemaVersion
+            );
+        }
+    }
+
+    register(
+        typeName: string,
+        objectType: { new (): T },
+        schemaVersion: Version = Versions.v1_0
+    ) {
+        let registrationInfo = this.findByName(typeName);
+
+        if (registrationInfo !== undefined) {
+            registrationInfo.objectType = objectType;
+        } else {
+            registrationInfo = {
+                typeName: typeName,
+                objectType: objectType,
+                schemaVersion: schemaVersion
+            };
+        }
+
+        this._items[typeName] = registrationInfo;
+    }
+
+    unregister(typeName: string) {
+        delete this._items[typeName];
+    }
+
+    createInstance(typeName: string, targetVersion: Version): T | undefined {
+        const registrationInfo = this.findByName(typeName);
+
+        return registrationInfo && registrationInfo.schemaVersion.compareTo(targetVersion) <= 0
+            ? new registrationInfo.objectType()
+            : undefined;
+    }
+
+    getItemCount(): number {
+        return Object.keys(this._items).length;
+    }
+
+    getItemAt(index: number): ITypeRegistration<T> {
+        return Object.keys(this._items).map((e) => this._items[e])[index];
+    }
+}
